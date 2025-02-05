a8fba478e330a7fe926932db9ca0c3dcc252d97e
Colby Conish <conish17913@gmail.com>
2025-02-05 12:19:17 -0500

    fixed dashboard to use telescope

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,13 +1,28 @@
 return {
   "folke/snacks.nvim",
-  priority = 1000,
+  dependencies = {'nvim-telescope/telescope.nvim'},
   lazy = false,
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+        preset = {
+            keys = {
+                { icon = " ", key = "f", desc = "Find File", action = ":Telescope find_files" },
+                { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                { icon = " ", key = "g", desc = "Find Text", action = ":Telescope live_grep" },
+                { icon = " ", key = "r", desc = "Recent Files", action = ":Telescope oldfiles" },
+                { icon = " ", key = "c", desc = "Config", action = function()
+                    require("telescope.builtin").find_files({cwd=vim.fn.stdpath('config')})
+                end},
+                { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+            },
+        },
+    },
     indent = { enabled = true },
     input = { enabled = true },
     picker = { enabled = true },
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
   tag = '0.1.5',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    local builtin = require('telescope.builtin') 
+    local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
